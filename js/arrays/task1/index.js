b6ff9d40fd3f8576b5c7b148b94649dafbcab65b
Vitaly Koshlyak <vitaly.koshlyak@gmail.com>
2022-11-04 11:36:35 +0200

    !

--- js/arrays/task1/index.js
+++ js/arrays/task1/index.js
@@ -27,3 +27,47 @@ const unshiftRes = numberList3.unshift(777);
 console.log(unshiftRes);
 
 console.log("AFTER UNSHIFT", numberList3);
+
+// refactored
+
+// input: number
+// output: boolean
+function isPrime(number) {
+  for (let index = 2; index < number; index += 1) {
+    if (number % index === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getPrimes(num) {
+  for (let number = 2; number <= num; number += 1) {
+    if (isPrime(number)) {
+      console.log(number);
+    }
+  }
+}
+
+getPrimes(10);
+getPrimes(11);
+getPrimes(15);
+
+// ..........................................
+const anotgerNumberList = [5, 0, 8, 10, -4, 50, 220];
+
+// input: callback
+// output: array
+
+// callback
+// input: callback
+// output: array
+
+/*
+const filterRes = anotgerNumberList.filter(function filterCallBack(el)) {
+    return el > 100;
+});
+console.log(filterRes);
+*/
+const filterRes = anotgerNumberList.filter((el) => el > 100);
+console.log(filterRes);
